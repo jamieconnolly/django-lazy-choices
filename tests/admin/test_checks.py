e7cd59bb8a462d280d50d757ec57a97f3c736f1c
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2016-02-29 22:06:40 +0000

    Add the running of ModelAdmin checks to the test compat module

--- tests/admin/test_checks.py
+++ tests/admin/test_checks.py
@@ -1,26 +1,27 @@
-from django.contrib import admin
+from django.contrib.admin.options import InlineModelAdmin as BaseInlineModelAdmin
 from django.core import checks
 from django.test import TestCase
 
 from lazychoices.admin import LazyChoiceInlineModelAdminMixin
 
+from ..compat import run_model_admin_check
 from .models import Book, Chapter
 
 
 class LazyChoiceInlineModelAdminChecksTests(TestCase):
     def test_lazy_model(self):
-        class InlineModelAdmin(LazyChoiceInlineModelAdminMixin, admin.options.InlineModelAdmin):
+        class InlineModelAdmin(LazyChoiceInlineModelAdminMixin, BaseInlineModelAdmin):
             lazy_model = Book
             model = Chapter
 
-        errors = InlineModelAdmin(Book, admin.site).check()
+        errors = run_model_admin_check(InlineModelAdmin, Book)
         self.assertEqual(errors, [])
 
     def test_missing_lazy_model(self):
-        class InlineModelAdmin(LazyChoiceInlineModelAdminMixin, admin.options.InlineModelAdmin):
+        class InlineModelAdmin(LazyChoiceInlineModelAdminMixin, BaseInlineModelAdmin):
             model = Chapter
 
-        errors = InlineModelAdmin(Book, admin.site).check()
+        errors = run_model_admin_check(InlineModelAdmin, Book)
         expected = [
             checks.Error(
                 "'tests.admin.test_checks.InlineModelAdmin' must have a 'lazy_model' attribute.",
@@ -32,11 +33,11 @@ class LazyChoiceInlineModelAdminChecksTests(TestCase):
         self.assertEqual(errors, expected)
 
     def test_invalid_lazy_model(self):
-        class InlineModelAdmin(LazyChoiceInlineModelAdminMixin, admin.options.InlineModelAdmin):
+        class InlineModelAdmin(LazyChoiceInlineModelAdminMixin, BaseInlineModelAdmin):
             lazy_model = Chapter
             model = Chapter
 
-        errors = InlineModelAdmin(Book, admin.site).check()
+        errors = run_model_admin_check(InlineModelAdmin, Book)
         expected = [
             checks.Error(
                 "The value of 'lazy_model' must inherit from 'LazyChoiceModelMixin'.",
