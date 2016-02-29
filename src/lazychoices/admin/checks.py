abee90a1c05da3463f12b7617e9005330195ac79
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2016-02-29 23:54:13 +0000

    Fix the compat issues for InlineModelAdmin between Django 1.8 and 1.9

--- src/lazychoices/admin/checks.py
+++ src/lazychoices/admin/checks.py
@@ -1,37 +1,15 @@
-from django.contrib.admin.checks import InlineModelAdminChecks
-from django.core import checks
+from django.contrib.admin.checks import must_be, must_inherit_from
 
+from lazychoices.compat import InlineModelAdminChecks
 from lazychoices.models import LazyChoiceModelMixin
 
 
 class LazyChoiceInlineModelAdminChecks(InlineModelAdminChecks):
-    def check(self, inline_obj, **kwargs):
-        errors = super(LazyChoiceInlineModelAdminChecks, self).check(inline_obj)
-        errors.extend(self._check_lazy_model(inline_obj))
-        return errors
-
     def _check_lazy_model(self, obj):
-        label = '.'.join([obj.__class__.__module__, obj.__class__.__name__])
-
         if not hasattr(obj, 'lazy_model'):
-            return [
-                checks.Error(
-                    "'{0}' must have a 'lazy_model' attribute.".format(label),
-                    hint=None,
-                    obj=obj.__class__,
-                    id='lazychoices.E101',
-                )
-            ]
+            return must_be('defined', option='lazy_model', obj=obj, id='lazychoices.E101')
         elif not issubclass(obj.lazy_model, LazyChoiceModelMixin):
-            return [
-                checks.Error(
-                    "The value of '{option}' must inherit from '{parent}'.".format(
-                        option='lazy_model', parent='LazyChoiceModelMixin'
-                    ),
-                    hint=None,
-                    obj=obj.__class__,
-                    id='lazychoices.E102',
-                )
-            ]
+            return must_inherit_from(parent='LazyChoiceModelMixin', option='lazy_model',
+                                     obj=obj, id='lazychoices.E102')
         else:
             return []
--- src/lazychoices/compat.py
+++ src/lazychoices/compat.py
@@ -1,4 +1,5 @@
 import django
+from django.contrib.admin.checks import InlineModelAdminChecks as BaseInlineModelAdminChecks
 
 
 def get_empty_value_display(model_admin):
@@ -7,3 +8,17 @@ def get_empty_value_display(model_admin):
     else:
         from django.contrib.admin.views.main import EMPTY_CHANGELIST_VALUE
         return EMPTY_CHANGELIST_VALUE
+
+
+if django.VERSION >= (1, 9):
+    class InlineModelAdminChecks(BaseInlineModelAdminChecks):
+        def check(self, inline_obj, **kwargs):
+            errors = super(InlineModelAdminChecks, self).check(inline_obj)
+            errors.extend(self._check_lazy_model(inline_obj))
+            return errors
+else:
+    class InlineModelAdminChecks(BaseInlineModelAdminChecks):
+        def check(self, cls, parent_model, **kwargs):
+            errors = super(InlineModelAdminChecks, self).check(cls, parent_model, **kwargs)
+            errors.extend(self._check_lazy_model(cls))
+            return errors
